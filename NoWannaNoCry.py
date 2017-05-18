6a7f067192370a11cca8a3b62ed5c301bab5e18a
thien phung <thienphg@gmail.com>
2017-05-18 10:08:07 +0700

    *) Sync from dolang/NoWannaNoCry

--- NoWannaNoCry.py
+++ NoWannaNoCry.py
@@ -7,9 +7,22 @@ if sys.platform != 'win32':
 if sys.getwindowsversion().platform != 2:
     sys.exit('Your Windows version is not supported by this script'
              ' (and probably not vulnerable).')
-if __name__ == '__main__':
-    try:
+def action():
+    name = raw_input('\r\nDo you want to continue? \r\n[D]isable SMB\r\n[F]ix SMB \r\n[A]ll\n')
+    if name.lower() == 'd':
         nwnc.mitigate()
+    elif name.lower() == 'f':
+        nwnc.fix()
+    elif name.lower() == 'a':
+        nwnc.mitigate()
+        nwnc.fix()
+    else:
+        action()
+if __name__ == '__main__':
+    print('NoWannaNoCry to help mitigate against the WCry/WannaCry malware.\nSource code: https://github.com/thienphung/NoWannaNoCry \n')
+    try:
+        if nwnc.check() is not True:
+            action()
         input('\r\nDone. Press any key to exit.')
     except Exception as e:
         sys.exit(e)
